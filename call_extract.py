de2ee640fb7d960f0b337a3396ec0905cc6bc32b
botniedata <botniedata@gmail.com>
2024-02-17 12:21:21 +0800

    removed call_extract.py file

--- call_extract.py
+++
@@ -1,5 +0,0 @@
-# call the function
-psgc = extract("F:\Data Engineering\psgc\datasets\PSGC-4Q-2023-Publication-Datafile.xlsx",
-               sheet = 3,
-               rows = None,
-               ind_col = None )
