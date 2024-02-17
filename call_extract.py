7401cfb040b15f39682439e5776e018619eb0aec
botniedata <botniedata@gmail.com>
2024-02-17 12:15:09 +0800

    rename psgc.py to extract.py and adds new file named call_extract_py

---
+++ call_extract.py
@@ -0,0 +1,5 @@
+# call the function
+psgc = extract("F:\Data Engineering\psgc\datasets\PSGC-4Q-2023-Publication-Datafile.xlsx",
+               sheet = 3,
+               rows = None,
+               ind_col = None )
