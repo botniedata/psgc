d5fbb6c496ccb50a2d73efed056b2c2709b37ba7
botniedata <botniedata@gmail.com>
2024-02-17 11:59:14 +0800

    run testing psgc.py file

--- psgc.py
+++ psgc.py
@@ -1,2 +1,34 @@
-# Hello World
-print("Hello World")
+# import packages pandas and numpy
+import pandas as pd
+import numpy as np
+
+# function to extract excel file as dataframe
+def extract(file_path, sheet, rows, ind_col):
+    
+    # read the file into memory
+    data = pd.read_excel(file_path, sheet_name = sheet, nrows = rows, index_col = ind_col)
+
+    # excel to df
+    data = pd.DataFrame(data)
+    
+    # printing details about the file
+    print(f"data store in [{file_path}]:")
+    print(f"\nNumber of rows [{data.shape[0]}], and Number of columns [{data.shape[1]}] in dataframe")
+    print(f"\nColumns in dataframe with it's data types: ")
+    
+    # print data types
+    print(data.dtypes)
+    
+    print(f"\n Printing the count value of NULL per column\n")
+    print(data.isna().sum())
+    
+    # print message before returning the dataframe
+    print(f"\nTo view the dataframe extracted from {file_path}, display the value returned by this function!\n\n") 
+    
+    return data
+
+# call the function
+psgc = extract("F:\Data Engineering\psgc\datasets\PSGC-4Q-2023-Publication-Datafile.xlsx",
+               sheet = 3,
+               rows = None,
+               ind_col = None )
